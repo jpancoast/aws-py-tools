223760e71f1efe311e83d559bc62031c2347199b
James Pancoast <jpancoast@gmail.com>
2014-12-20 21:34:36 -0700

    blah

--- createSnapShot.py
+++ createSnapShot.py
@@ -5,16 +5,42 @@ import sys
 import signal
 import yaml
 
-from lib.AWSPyTools import ParseOptions
+#from lib.AWSPyTools import ParseOptions
 from lib.AWSPyTools import AWSPyTools
 
 
+from docopt import docopt
+
+
+""" docopt_example.py
+    
+    Usage:
+        docopt_example.py -h
+        docopt_example.py <required> [-f | -g | -o ]
+        docopt_example.py <repeating>...
+    Options:
+        -h,--help       : show this help message
+        required        : example of a required argument
+        repeating       : example of repeating arguments
+        -f,--flag       : example flag #1
+        -g,--greatflag  : example of flag #2
+        -o,--otherflag  : example of flag #3
+"""
+
+""" createSnapShot.py
+    
+    Usage:
+        createSnapShot.py -h | -r | -e
+    Options:
+        -h,--help       : help message
+
+"""
 
 def main(argv):
     signal.signal(signal.SIGINT, signal_handler)
 
-    po = ParseOptions(sys.argv)
-    (env, region, vpc_id) = po.getAwsOptions()
+#    po = ParseOptions(sys.argv)
+#    (env, region, vpc_id) = po.getAwsOptions()
 
     debug = True
 
@@ -25,6 +51,7 @@ def main(argv):
     #vols = conn.get_all_volumes(filters={ 'tag:' + config['tag_name']: config['tag_value'] })
     for vol in awsPyTools.getVolumes():
         print vol
+        print
 
 def signal_handler(signal, frame):
     print sys.argv[0] + " exited via keyboard interrupt."
@@ -32,4 +59,5 @@ def signal_handler(signal, frame):
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    args = docopt(__doc__)
+    main(args)
