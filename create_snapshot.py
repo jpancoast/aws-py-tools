e27ba399818648d32c7cf8e1ad44e06d0508a8fa
James Pancoast <jpancoast@gmail.com>
2015-09-11 21:55:03 -0600

    on my way there

--- create_snapshot.py
+++ create_snapshot.py
@@ -5,13 +5,14 @@
 Create a snapshot for a given volume id.
 
 Usage:
-    create_snapshot.py (--vol_id=<vol_id>) (--profile=<profile>)
+    create_snapshot.py (--vol_id=<vol_id>) (--profile=<profile>) (--region=<region>)
 
 Options:
     --help  Show this screen
     --version  Show version
     -v, --vol_id=<vol_id> Volume ID to make a snapshot of
     -p, --profile=<profile_name> Boto profile to use to connect. If not given, will attempt to use ENV variables.
+    -r, --region=<region> Region
 """
 
 VERSION = '0.1'
@@ -19,6 +20,9 @@ VERSION = '0.1'
 import os
 import sys
 import pprint
+import datetime
+
+import boto.ec2
 
 
 try:
@@ -43,7 +47,12 @@ def main(argv):
 
     profile = None
     vol_id = None
+    region = None
 
+#    real_user_time = datetime.datetime.strptime(user_time, '%Y-%m-%d %H:%M:%S')
+    formatted_desc_date = datetime.datetime.now().strftime('%m/%d/%Y')
+    description = "Compnor Data Volume Backup " + formatted_desc_date
+    print "Description: " + description
 
     if arguments['--vol_id'] is not None:
         vol_id = arguments['--vol_id']
@@ -51,12 +60,39 @@ def main(argv):
     if arguments['--profile'] is not None:
         profile = arguments['--profile']
 
+    if arguments['--region'] is not None:
+        region = arguments['--region']
 
     print "---------------"
     print "Boto Profile: " + str(profile)
     print "Volume ID: " + str(vol_id)
+    print "Region: " + str(region)
     print "---------------\n"
 
+    ec2_conn = boto.ec2.connect_to_region(region, profile_name=profile)
+
+    try:
+        volumes = ec2_conn.get_all_volumes(volume_ids=[vol_id])
+    except boto.exception.EC2ResponseError, e:
+        print "There was an ec2 error: "
+        print "=========================="
+        print e
+        print "=========================="
+        exit()
+
+
+    for volume in volumes:
+        pp.pprint(volume.id)
+
+#        volume.create_snapshot(description="This is a test description")
+
+        '''
+        for snapshot in volume.snapshots():
+            pp.pprint(snapshot)
+            print snapshot.start_time
+            print snapshot.description
+            print "---------------"
+        '''
 
 if __name__ == "__main__":
     main(sys.argv)
